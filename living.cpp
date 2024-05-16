d45e8f332e2c5d204a27a46f0f1c3c0a5ebb71b4
Manavmehr <a1886221@adelaide.edu.au>
2024-05-16 16:12:42 +0930

    living

---
+++ living.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class living {
+  public:
+    string name;
+    bool is_zookeeper;
+
+}
+
+
+class living_Zookeeper:public living{
+    public:
+ double hourly_rate;
+}
+   
+
+
+class living_animal:public living{
+public:
+int hunger_level;
+int happiness_level;
+bool is_dead;
+int visitors_per_hour;
+double height;
+double weight;
+
+void hunger_rate(height,weight){
+
+float hungerrate=(height*weight)/100;
+return hungerrate 
+
+}
+
+void hunger_happines_relationship(hunger_level){
+if hunger_level<50 
+while(hunger_level<50){
+
+    happiness_level-1
+
+    cout << happiness_level;
+    return 0;
+}
+
+
+}
+void cleanliness_happinesss_relationship(){
+
+while(cleanliness<50){
+happiness_level-1
+cout<< happiness_level;
+return 0
+}
+}
+
+void feed(food)[
+hunger_level=100
+
+]
+
+}
+class living_animal_mammal:public living_animal {
+public:
+bool need_shelter;
+
+void mammal_cleanliness(height,weight){
+    (height + weight)/100;
+    return:0
+
+}
+
+
+}
+
+
