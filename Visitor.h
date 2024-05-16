76eb9c12d749cee7cb8cbbf42c41dfd1743a95f5
Elias Klestinis <eliasklestinis@Eliass-MacBook-Pro.local>
2024-05-16 14:39:38 +0930

    changes

---
+++ Visitor.h
@@ -0,0 +1,28 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+
+#include "Enclosure.h" // Make sure this gels well with these files
+
+class Visitor {
+private:
+    int satisfaction_level;
+    int money_spent;
+
+public:
+    // Constructor
+    Visitor();
+
+    // Getters and setters for class attributes
+    int get_Satisfaction_Level() const;
+    void set_Satisfaction_Level(int level);
+
+    int get_Money_Spent() const;
+    void set_Money_Spent(int amount);
+
+    // Extra class functions
+    void visit_Enclosure(Enclosure* enclosure);
+
+    void make_Purchase(double dollar_amount);
+};
+
+#endif
