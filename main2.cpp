757dfa06b315e3320a1985c38c06ffe3df92f3ca
Elias Klestinis <eliasklestinis@Eliass-MBP.modem>
2024-05-23 23:18:58 +0930

    fixing bugs

---
+++ main2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int option1;
+
+
+void main_menu(){
+    cout << "\nWelcome to Zoo management simulator!\n";
+    cout <<"1. Open zoo for the day\n";
+    cout <<"2. Exit\n";
+    cout <<"\nEnter your choice: \n";
+}
+
+void option_1_menu(){
+    cout << "\nWelcome to Zoo management simulator!\n";
+    cout <<"1. Open zoo for the day\n";
+    cout <<"2. Exit\n";
+    cout <<"\nEnter your choice: \n";
+}
+
+
+
+
+
+cin >> option1;
+
+
+
+
+void opened_zoo_menu(){
+    cout <<"1.Manage Animals\n";
+cout <<"2.Manage enclosures\n";
+cout<<"3.Manage Staff\n";
+cout <<"4.Close zoo for the day\n";
+cout <<"Enter your choice:/n";
+
+}
+void Manage_animal(){
+cout<<"1.list all Animals\n";
+cout<<"2."
+
+}
+
+
+int main(){
+
+
+     int option_main;
+     int option_
+     bool running = true;
+switch(option){
+
+    case 1:
+    cout<<"Zoo has been openened for the day/n";
+    cout<< Visitor::visitors() << "Visitors have been admitted\n";
+    opened_zoo_menu();
+        switch(option){
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            case 4:
+
+
+
+        }
+
+    break; 
+
+    case 2:
+
+
+}
+
+
+
+}
